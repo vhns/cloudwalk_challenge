082cf20fde38e0e5b2a3039ffec9fd5c28ce8a2a
Vitor Hugo Nunes dos Santos <vitorhugonunes@proton.me>
2026-04-04 15:59:17 -0300

    Initial commit and basic import functionality.
    Include the basic functionality to generate a sqlite db, on which
    we'll run queries to generate stats later on.

---
+++ tasks/hands_dirty/import.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import sys
+import sqlite3
+import csv
+
+def import_data(csv_data, cur, name):
+    file = None
+    with open(csv_data, mode='r', newline='') as f: file = f.read().splitlines()
+    file.pop(0)
+    reader = csv.reader(file, delimiter=',')
+    f = [tuple(i) for i in reader]
+    cur.execute(f"CREATE TABLE IF NOT EXISTS {name} (\
+                    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\
+                    time INT NOT NULL,\
+                    today INT NOT NULL,\
+	                yesterday INT NOT NULL,\
+	                last_week INT NOT NULL,\
+	                avg_last_week FLOAT NOT NULL,\
+	                avg_last_month FLOAT NOT NULL)")
+    cur.executemany(f"INSERT INTO {name} \
+                        VALUES(NULL, ?, ?, ?, ?, ?, ?)",
+                    f)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 3:
+        print('Too many arguments.')
+        os.exit()
+    if len(sys.argv) < 3:
+        print('Too few arguments.')
+        os.exit()
+    con = sqlite3.connect(sys.argv[1])
+    cur = con.cursor()
+    name = ''.join(sys.argv[2].split('.')[0:-1])
+    for i in sys.argv[2:]:
+        import_data(i, cur, name)
+    con.commit()
